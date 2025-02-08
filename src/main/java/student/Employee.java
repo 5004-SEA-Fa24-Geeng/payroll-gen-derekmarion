1b115ac91194e8db923f5eea71c66140c80cb6ca
Derek Marion <github.magnify953@passmail.net>
2025-02-08 12:15:37 -0800

    docs: remove duplicate comments

--- src/main/java/student/Employee.java
+++ src/main/java/student/Employee.java
@@ -62,48 +62,6 @@ public class Employee implements IEmployee {
         return this.pretaxDeductions;
     }
 
-    /**
-     * Runs the employee's payroll.
-     * 
-     * This will calculate the pay for the current pay, update the YTD earnings, and
-     * update the
-     * taxes paid YTD.
-     * 
-     * taxes are calculated as 1.45% for medicare, 6.2% for social security, and 15%
-     * for
-     * withholding. or 22.65% total. They are calculated on the net pay (after
-     * pretax deductions).
-     * 
-     * For hourly employees, the pay is calculated as payRate * hoursWorked for the
-     * first 40 hours,
-     * then payRate * 1.5 * (hoursWorked - 40) for overtime.
-     * 
-     * For salary employees, it is pay rate divided by 24 for two payments every
-     * month.
-     * 
-     * If either type of employee has < 0 hours, they are skipped this payroll
-     * period.
-     * (suggestion return null, and skip adding nulls to your paystub list)
-     * 
-     * Final net pay is calculated as pay - pretaxDeductions - taxes.
-     * 
-     * All numbers (across all methods) are rounded to the nearest cent. (2 decimal
-     * places)
-     * 
-     * SUGGESTION: You may want to use BigDecimal for the calculations to avoid
-     * floating point errors.
-     * SUGGESTION: You may want to create an protected abstract
-     * calculateGrossPay(double hoursWorked)
-     * method to calculate the gross pay for the pay period, as runPayroll is
-     * exactly
-     * the same for both SalaryEmployee and HourlyEmployee, but calculateGrossPay is
-     * different.
-     * 
-     * @param hoursWorked the hours worked for the pay period
-     * 
-     * @return the pay stub for the current pay period
-     * 
-     */
     @Override
     public PayStub runPayroll(double hoursWorked) {
         if (hoursWorked < 0) {
