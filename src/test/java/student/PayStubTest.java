10a9cf4b8be2e7db067d456fb07f030e78aadf93
Derek Marion <github.magnify953@passmail.net>
2025-02-08 12:21:02 -0800

    test: add tests for PayStub

--- src/test/java/student/PayStubTest.java
+++ src/test/java/student/PayStubTest.java
@@ -1,31 +1,49 @@
-// package student;
+import student.PayStub;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-// import student.PayStub;
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-// import org.junit.jupiter.api.BeforeEach;
-// import org.junit.jupiter.api.Test;
+public class PayStubTest {
 
-// public class PayStubTest {
+    private PayStub payStub;
 
-//     private PayStub payStub;
+    @BeforeEach
+    public void setUp() {
+        payStub = new PayStub("John Doe", 20000, 5000, 100000, 25000);
+    }
 
-//     @BeforeEach
-//     public void setUp() {
-//         payStub = new PayStub(20000, 5000);
-//     }
+    @Test
+    public void testGetPay() {
+        assertEquals(20000, payStub.getPay());
+    }
 
-//     @Test
-//     public void testGetPay() {
-//         assertEquals(20000, payStub.getPay());
-//     }
+    @Test
+    public void testGetTaxesPaid() {
+        assertEquals(5000, payStub.getTaxesPaid());
+    }
 
-//     @Test
-//     public void testGetTaxesPaid() {
-//         assertEquals(5000, payStub.getTaxesPaid());
-//     }
+    @Test
+    public void testGetNetPay() {
+        assertEquals(20000, payStub.getNetPay());
+    }
 
-//     // @Test
-//     // public void testToCSV() {
-//     // assertEquals("20000,5000", payStub.toCSV());
-//     // }
-// }
+    @Test
+    public void testGetEmployeeName() {
+        assertEquals("John Doe", payStub.getEmployeeName());
+    }
+
+    @Test
+    public void testGetYTDEarnings() {
+        assertEquals(100000, payStub.getYTDEarnings());
+    }
+
+    @Test
+    public void testGetYTDTaxesPaid() {
+        assertEquals(25000, payStub.getYTDTaxesPaid());
+    }
+
+    @Test
+    public void testToCSV() {
+        assertEquals("John Doe,20000.00,5000.00,100000.00,25000.00", payStub.toCSV());
+    }
+}
